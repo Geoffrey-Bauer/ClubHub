785c82ba08e87f354170e7ecc74d92294fd5a8e5
Geoffrey-Bauer <geoffrey.pro@mailfence.com>
2024-07-08 14:13:32 +0200

    Docker Clean

---
+++ App/src/Controller/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AuthController extends AbstractController
+{
+    #[Route('/auth', name: 'app_auth')]
+    public function index(): Response
+    {
+        return $this->render('auth/index.html.twig', [
+            'controller_name' => 'AuthController',
+        ]);
+    }
+}
